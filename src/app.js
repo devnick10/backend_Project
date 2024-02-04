83350616ac8c053611c50adf1cad12f702bfde60
devnick10 <nickbhoyar683@gmail.com>
2024-02-04 16:42:45 +0530

    utils code

--- src/app.js
+++ src/app.js
@@ -1,1 +1,19 @@
- 
+import express from "express";
+import cores from "cores";
+import cookieParser from "cookie-parser";
+const app = express();
+
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    Credential: true,
+  })
+);
+
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"))
+app.use(cookieParser())
+
+
+export { app };
