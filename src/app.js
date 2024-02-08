af71d5c57470de2591adbe8b3a2abf44ea1b397b
devnick10 <nickbhoyar683@gmail.com>
2024-02-09 01:07:42 +0530

    setup configured

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
-import cores from "cores";
+import cors from "cors";
 import cookieParser from "cookie-parser";
+
 const app = express();
 
 app.use(
@@ -12,8 +13,13 @@ app.use(
 
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
-app.use(express.static("public"))
-app.use(cookieParser())
+app.use(express.static("public"));
+app.use(cookieParser());
 
+// routes Imports
 
+import userRouter from "./routes/user.routes.js";
+
+// routes declaration
+app.use("/api/v1/users", userRouter);
 export { app };
